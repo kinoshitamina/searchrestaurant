4fd3a353d0a78581903ae0eb316f4384fbdbf9e5
木下美奈 <@example.com>
2026-09-08 11:49:03 +0900

    Mainクラスに店舗追加と一覧表示処理を追加

--- src/searchrestaurant/Main.java
+++ src/searchrestaurant/Main.java
@@ -6,6 +6,10 @@ public class Main {
 		Restaurant restaurant = new Restaurant(1, "シュラスコ&ビアレストランALEGRIA 五反田", "シュラスコ・肉", 1000, 1999, 4,
 				"東京都品川区西五反田1-30-2 ウイン五反田 B1F", "年末年始", "平日: 17:00~22:30 土日祝: 11:30~15:00 (ディナー) 16:00~22:30", 3.2);
 		restaurant.showInfo();
-	}
 
+		RestaurantSearch restaurantSearch = new RestaurantSearch();
+		restaurantSearch.addRestaurant(restaurant);
+		restaurantSearch.showAllRestaurants();
+
+	}
 }
