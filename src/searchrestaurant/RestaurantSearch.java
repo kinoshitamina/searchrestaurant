d41a38d0c9ac84db6e4dc0e12158a21ce36bba4d
木下美奈 <@example.com>
2026-09-08 11:38:12 +0900

    RestaurantSearchクラスを作成し店舗検索機能を追加

---
+++ src/searchrestaurant/RestaurantSearch.java
@@ -0,0 +1,65 @@
+package searchrestaurant;
+
+import java.util.ArrayList;
+
+//import apple.laf.JRSUIState.TitleBarHeightState;
+
+public class RestaurantSearch {
+	private ArrayList<Restaurant> restaurants;
+
+	public RestaurantSearch() {
+		restaurants = new ArrayList<Restaurant>();
+	}
+
+	public void addRestaurant(Restaurant restaurant) {
+		restaurants.add(restaurant);
+	}
+
+	public void showAllRestaurants() {
+		for (Restaurant restaurant : restaurants) {
+			restaurant.showInfo();
+		}
+	}
+
+	public void searchByGenre(String genre) {
+		for (Restaurant restaurant : restaurants) {
+			if (genre.equals(restaurant.getGenre())) {
+				restaurant.showInfo();
+			}
+		}
+	}
+
+	public void searchByTitle(String title) {
+		for (Restaurant restaurant : restaurants) {
+			if (title.equals(restaurant.getTitle())) {
+				restaurant.showInfo();
+			}
+		}
+
+	}
+
+	public void searchByMaxBudget(int maxBudget) {
+		for (Restaurant restaurant : restaurants) {
+			if (restaurant.getMinPrice() <= maxBudget) {
+				restaurant.showInfo();
+			}
+		}
+	}
+
+	public void searchByWalkMinutes(int walkMinutes) {
+		for (Restaurant restaurant : restaurants) {
+			if (restaurant.getWalkMinutes() <= walkMinutes) {
+				restaurant.showInfo();
+			}
+		}
+	}
+
+	public void searchByRating(double rating) {
+		for (Restaurant restaurant : restaurants) {
+			if (restaurant.getRating() >= rating) {
+				restaurant.showInfo();
+			}
+		}
+	}
+
+}
