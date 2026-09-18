d0c9cc48848921dc8668f9e9c59ef201b5ddb060
木下美奈 <kinoshitamina060403@gmail.com>
2026-09-18 17:09:12 +0900

    店舗データを30件に追加

--- src/searchrestaurant/RestaurantData.java
+++ src/searchrestaurant/RestaurantData.java
@@ -7,7 +7,32 @@ public class RestaurantData {
 		restaurantSearch.addRestaurant(restaurant2);
 		restaurantSearch.addRestaurant(restaurant3);
 		restaurantSearch.addRestaurant(restaurant4);
-		restaurantSearch.addRestaurant(restaurant5);
+		restaurantSearch.addRestaurant(restaurant6);
+		restaurantSearch.addRestaurant(restaurant7);
+		restaurantSearch.addRestaurant(restaurant8);
+		restaurantSearch.addRestaurant(restaurant9);
+		restaurantSearch.addRestaurant(restaurant10);
+		restaurantSearch.addRestaurant(restaurant11);
+		restaurantSearch.addRestaurant(restaurant12);
+		restaurantSearch.addRestaurant(restaurant14);
+		restaurantSearch.addRestaurant(restaurant14);
+		restaurantSearch.addRestaurant(restaurant15);
+		restaurantSearch.addRestaurant(restaurant16);
+		restaurantSearch.addRestaurant(restaurant17);
+		restaurantSearch.addRestaurant(restaurant18);
+		restaurantSearch.addRestaurant(restaurant19);
+		restaurantSearch.addRestaurant(restaurant20);
+		restaurantSearch.addRestaurant(restaurant21);
+		restaurantSearch.addRestaurant(restaurant22);
+		restaurantSearch.addRestaurant(restaurant23);
+		restaurantSearch.addRestaurant(restaurant24);
+		restaurantSearch.addRestaurant(restaurant25);
+		restaurantSearch.addRestaurant(restaurant26);
+		restaurantSearch.addRestaurant(restaurant27);
+		restaurantSearch.addRestaurant(restaurant28);
+		restaurantSearch.addRestaurant(restaurant29);
+		restaurantSearch.addRestaurant(restaurant30);
+
 	}
 
 	Restaurant restaurant1 = new Restaurant(1, "シュラスコ&ビアレストランALEGRIA 五反田", "シュラスコ・肉", 1000, 1999, 4,
@@ -20,5 +45,61 @@ public class RestaurantData {
 			"なし", "ランチタイム: 11:00 ～ 14:30、ディナータイム: 16:30 ～ 24:00 ");
 	Restaurant restaurant5 = new Restaurant(5, "串カツ田中 五反田駅前店", "串揚げ、居酒屋、うどん", 2000, 2900, 2,
 			"東京都品川区東五反田1-12-5 秀栄ビル1", "なし", "平日：16:00 〜 23:00 / 0:00 頃、土日祝：12:00 〜 23:00 / 0:00 / 深夜1:00 頃");
+	Restaurant restaurant6 = new Restaurant(6, "魚政　五反田", "居酒屋・海鮮", 4001, 5000, 5,
+			"東京都品川区東五反田1-12-10 エイチ・エフ五反田駅前ビル 5F-A ",
+			"なし", "月～木、祝日: 15:00～翌0:00 金、祝前日: 14:00～翌0:00 土、日: 12:00～翌0:00");
+	Restaurant restaurant7 = new Restaurant(7, "しゃぶしゃぶ温野菜 五反田店別館", "和食", 1500, 4000, 2, "東京都品川区西五反田1-5-2 トラヤビル8F",
+			"年末年始", "月～日、祝日、祝前日: 16:00～23:00");
+	Restaurant restaurant8 = new Restaurant(8, "大衆イタリアン酒場 ばくりこ", "イタリアン", 2000, 2900, 5, "東京都品川区西五反田２丁目６−３",
+			"なし", "月~金・祝前日:17:00 - 23:00 土日祝: 15:00 - 23:00");
+	Restaurant restaurant9 = new Restaurant(9, "手羽先酒場 幸の羽 五反田本店", "手羽先・肉・居酒屋", 3000, 3900, 3, "東京都品川区西五反田1-12-1 桜小路",
+			"日曜日", "月曜〜土曜・祝日・祝前日: 16:00 ～ 翌3:00");
+	Restaurant restaurant10 = new Restaurant(10, "やきとり 糸", "焼き鳥", 8000, 9000, 3, "東京都品川区西五反田１丁目２５−１ B1",
+			"火曜日", "17:00 ～ 23:00");
+	Restaurant restaurant11 = new Restaurant(11, "鮨 琥珀", "寿司・海鮮", 3000, 3900, 4, "東京都品川区西五反田１丁目１８−１ Gotanda ex 6F",
+			"なし", "11時30分～14時00分, 17時00分～23時00分");
+	Restaurant restaurant12 = new Restaurant(12, "グリル エフ", "洋食", 1000, 2000, 1, "東京都品川区東五反田１丁目１３−９",
+			"日曜日・祝日", "11:00 ～ 14:30 17:00 ～ 21:00");
+	Restaurant restaurant13 = new Restaurant(13, "なるとキッチン 五反田店", "唐揚げ・焼き鳥・肉", 1000, 1900, 5, " 東京都品川区西五反田２丁目２６−４",
+			"日曜日", "平日: 11:30〜15:00と17:00〜23:00、土曜・祝日: 16:00〜23:00");
+	Restaurant restaurant14 = new Restaurant(14, "SOSTA - PIZZA & LEGAME -", "イタリアン", 5000, 5900, 3,
+			"東京都品川区西五反田１丁目３２−９",
+			"日曜日", "ランチ：11:30 ～ 15:00 ディナー：17:30 ～ 23:30");
+	Restaurant restaurant15 = new Restaurant(15, "もつ焼き 五反田 ふじ屋", "居酒屋・もつ鍋", 3000, 3900, 5,
+			"東京都品川区西五反田１丁目２６−２ 五反田サンハイツ101",
+			"なし", "月曜～木曜: 16:00 ～ 24:00 金曜: 16:00 ～ 25:00 土曜・日曜: 15:00 ～ 24:00 ");
+	Restaurant restaurant16 = new Restaurant(16, "七輪焼肉 安安 五反田西口店", "焼肉", 2000, 2900, 1, "東京都品川区西五反田2-7-8 誠実ビル 2F",
+			"なし", "月～金：17:00～翌4:30 土・祝日：16:00～翌4:30 日：16:00～23:00");
+	Restaurant restaurant17 = new Restaurant(17, "ラーメン魁力屋 五反田店", "ラーメン", 1000, 1900, 1, " 東京都品川区西五反田１丁目８−１",
+			"なし", "10:30 ～ 25:00");
+	Restaurant restaurant18 = new Restaurant(18, "一風堂 五反田東口店", "ラーメン", 1000, 1900, 1, "東京都品川区東五反田１丁目１４−１４ 北原ビル 2F",
+			"なし", "月曜日～金曜日：11:00～22:00 土曜日・日曜日：10:30～22:00");
+	Restaurant restaurant19 = new Restaurant(19, "つけめんTETSU 五反田店", "つけ麺・ラーメン", 1000, 1900, 1, "東京都品川区東五反田２丁目１−１",
+			"なし", "月〜土・祝前日: 11:00〜翌5:00 日・祝: 11:00〜23:00");
+	Restaurant restaurant20 = new Restaurant(20, "スシロー 五反田店", "寿司", 2000, 3000, 3, "東京都品川区西五反田１丁目３−８ 五反田Placeビル B1",
+			"なし", "月曜日～金曜日：11:00～23:00 土日祝日：10:30～23:00");
+	Restaurant restaurant21 = new Restaurant(21, "ラーメン豚山 五反田店", "ラーメン", 600, 1000, 1, "東京都品川区東五反田１丁目２６−６",
+			"なし", "11時00分～2時00分");
+	Restaurant restaurant22 = new Restaurant(22, "天下一品 五反田店", "ラーメン・中華", 600, 1000, 4, "東京都品川区西五反田1-25-5 松村ビル",
+			"なし", "月〜木：11:00〜翌2:00 金・祝前日：11:00〜翌3:00 土・日・祝：11:00〜23:00");
+	Restaurant restaurant23 = new Restaurant(23, "ミート矢澤", "肉・ハンバーグ", 2000, 7000, 5, "東京都品川区西五反田２丁目１５−１３ ニューハイツ西五反田 1F",
+			"年末年始", "[平日]ランチ 11:00～15:00 [土・日・祝] 11:00～23:30");
+	Restaurant restaurant24 = new Restaurant(24, "陳家私菜 五反田店", "中華", 1000, 2000, 3, "東京都品川区西五反田2-26-2 桔梗ハイツ2F",
+			"日曜日", "平日: ランチ 11:30～15:00 / ディナー 17:30～23:00 土曜・祝日: ランチ 11:30～15:00 / ディナー 17:30～21:30");
+	Restaurant restaurant25 = new Restaurant(25, "ONIGIRI CAFE ひとにぎり 五反田東急スクエア", "和食・おにぎり", 1000, 2000, 1,
+			"東京都品川区東五反田2-1-2 五反田東急スクエア 4F",
+			"五反田東急スクエアに準ずる", "08:00 ～ 22:00");
+	Restaurant restaurant26 = new Restaurant(26, "アッパオンマ", "韓国料理", 3000, 4000, 3, "西五反田１丁目３０−２ ウィン五反田ビル 1F",
+			"なし", "月～日、祝日、祝前日: 17:00～翌5:00 ディナータイム：17:00～22:00");
+	Restaurant restaurant27 = new Restaurant(27, "焼肉もとやま 五反田店", "焼肉", 6000, 7900, 1, "141-0031 東京都品川区西五反田１丁目２−８ 4F",
+			"なし", "16:00～23:00");
+	Restaurant restaurant28 = new Restaurant(28, "とんちゃん 五反田店", "焼肉・韓国料理", 3000, 3900, 2,
+			"東京都品川区東五反田５丁目２８−１２ EX五反田ビル ２F",
+			"なし", "7:00～翌5:00");
+	Restaurant restaurant29 = new Restaurant(29, "サイゼリア", "イタリアン", 1000, 2000, 4, "東京都品川区西五反田１丁目１１−１ Aios 五反田駅前 2F",
+			"なし", "11:00～翌5:00");
+	Restaurant restaurant30 = new Restaurant(30, "マクドナルド", "ハンバーガー・ファーストフード", 1, 1000, 1,
+			"東京都品川区東五反田１丁目２６−１４ アトレ五反田1 2F・3F",
+			"なし", "24時間営業");
 
 }
