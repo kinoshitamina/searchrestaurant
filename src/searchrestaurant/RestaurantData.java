2725742aafb33d1c04199121adc963be1d2b94f6
木下美奈 <kinoshitamina060403@gmail.com>
2026-09-24 23:27:51 +0900

    店舗の予算データを見直し

--- src/searchrestaurant/RestaurantData.java
+++ src/searchrestaurant/RestaurantData.java
@@ -36,70 +36,70 @@ public class RestaurantData {
 
 	}
 
-	Restaurant restaurant1 = new Restaurant(1, "シュラスコ&ビアレストランALEGRIA 五反田", "シュラスコ・肉", 1000, 1999, 4,
+	Restaurant restaurant1 = new Restaurant(1, "シュラスコ&ビアレストランALEGRIA 五反田", "シュラスコ・肉", 3000, 4000, 4,
 			"東京都品川区西五反田1-30-2 ウイン五反田 B1F", "年末年始", "平日: 17:00~22:30 土日祝: 11:30~15:00 (ディナー) 16:00～22:30");
-	Restaurant restaurant2 = new Restaurant(2, "焼き鳥 しょうちゃん 五反田店", "居酒屋・肉", 3000, 3900, 1,
+	Restaurant restaurant2 = new Restaurant(2, "焼き鳥 しょうちゃん 五反田店", "居酒屋・肉", 4000, 4999, 1,
 			"東京都品川区東五反田1丁目16−9 フェニックスビル 1F", "日曜日", "月・火・水・木・祝日: 17:00～翌5:00、金曜日: 17:00～翌8:00、土・祝前日: 16:00～翌5:00");
-	Restaurant restaurant3 = new Restaurant(3, "福祥餃子楼", "居酒屋・中華", 1000, 3900, 4, "東京都品川区西五反田2-26-4 I・A・BLDG 1F",
+	Restaurant restaurant3 = new Restaurant(3, "福祥餃子楼", "居酒屋・中華", 600, 800, 4, "東京都品川区西五反田2-26-4 I・A・BLDG 1F",
 			"なし", "ランチ：11:30～15:00、ディナー：17:00～23:00");
-	Restaurant restaurant4 = new Restaurant(4, "ウミボウズ 五反田駅前店", "海鮮・居酒屋", 1000, 3900, 2, "東京都品川区西五反田2-7-8 誠實ビル 7F",
+	Restaurant restaurant4 = new Restaurant(4, "ウミボウズ 五反田駅前店", "海鮮・居酒屋", 501, 1000, 2, "東京都品川区西五反田2-7-8 誠實ビル 7F",
 			"なし", "ランチタイム: 11:00～14:30、ディナータイム: 16:30～24:00");
-	Restaurant restaurant5 = new Restaurant(5, "串カツ田中 五反田駅前店", "串揚げ・居酒屋・うどん", 2000, 2900, 2,
+	Restaurant restaurant5 = new Restaurant(5, "串カツ田中 五反田駅前店", "串揚げ・居酒屋・うどん", 2000, 2999, 2,
 			"東京都品川区東五反田1-12-5 秀栄ビル1", "なし", "平日：16:00～23:00 / 0:00 頃、土日祝：12:00～23:00 / 0:00 / 深夜1:00 頃");
-	Restaurant restaurant6 = new Restaurant(6, "魚政 五反田", "居酒屋・海鮮", 4001, 5000, 5,
+	Restaurant restaurant6 = new Restaurant(6, "魚政 五反田", "居酒屋・海鮮", 4000, 4999, 5,
 			"東京都品川区東五反田1-12-10 エイチ・エフ五反田駅前ビル 5F-A",
 			"なし", "月～木、祝日: 15:00～翌0:00 金、祝前日: 14:00～翌0:00 土、日: 12:00～翌0:00");
-	Restaurant restaurant7 = new Restaurant(7, "しゃぶしゃぶ温野菜 五反田店別館", "和食", 1500, 4000, 2, "東京都品川区西五反田1-5-2 トラヤビル8F",
+	Restaurant restaurant7 = new Restaurant(7, "しゃぶしゃぶ温野菜 五反田店別館", "和食", 3000, 3999, 2, "東京都品川区西五反田1-5-2 トラヤビル8F",
 			"年末年始", "月～日、祝日、祝前日: 16:00～23:00");
-	Restaurant restaurant8 = new Restaurant(8, "大衆イタリアン酒場 ばくりこ", "イタリアン", 2000, 2900, 5, "東京都品川区西五反田2丁目6−3",
+	Restaurant restaurant8 = new Restaurant(8, "大衆イタリアン酒場 ばくりこ", "イタリアン", 2000, 2999, 5, "東京都品川区西五反田2丁目6−3",
 			"なし", "月~金・祝前日:17:00 - 23:00 土日祝: 15:00 - 23:00");
-	Restaurant restaurant9 = new Restaurant(9, "手羽先酒場 幸の羽 五反田本店", "手羽先・肉・居酒屋", 3000, 3900, 3, "東京都品川区西五反田1-12-1 桜小路",
+	Restaurant restaurant9 = new Restaurant(9, "手羽先酒場 幸の羽 五反田本店", "手羽先・肉・居酒屋", 3000, 3999, 3, "東京都品川区西五反田1-12-1 桜小路",
 			"日曜日", "月曜〜土曜・祝日・祝前日: 16:00～翌3:00");
-	Restaurant restaurant10 = new Restaurant(10, "やきとり 糸", "焼き鳥", 8000, 9000, 3, "東京都品川区西五反田1丁目25−1 B1",
+	Restaurant restaurant10 = new Restaurant(10, "やきとり 糸", "焼き鳥", 8000, 9999, 3, "東京都品川区西五反田1丁目25−1 B1",
 			"火曜日", "17:00～23:00");
-	Restaurant restaurant11 = new Restaurant(11, "鮨 琥珀", "寿司・海鮮", 3000, 3900, 4, "東京都品川区西五反田1丁目18−1 Gotanda ex 6F",
+	Restaurant restaurant11 = new Restaurant(11, "鮨 琥珀", "寿司・海鮮", 3000, 3999, 4, "東京都品川区西五反田1丁目18−1 Gotanda ex 6F",
 			"なし", "11時30分～14時00分, 17時00分～23時00分");
-	Restaurant restaurant12 = new Restaurant(12, "グリル エフ", "洋食", 1000, 2000, 1, "東京都品川区東五反田1丁目13−9",
+	Restaurant restaurant12 = new Restaurant(12, "グリル エフ", "洋食", 1000, 1999, 1, "東京都品川区東五反田1丁目13−9",
 			"日曜日・祝日", "11:00 ～ 14:30 17:00 ～ 21:00");
-	Restaurant restaurant13 = new Restaurant(13, "なるとキッチン 五反田店", "唐揚げ・焼き鳥・肉", 1000, 1900, 5, "東京都品川区西五反田2丁目26−4",
+	Restaurant restaurant13 = new Restaurant(13, "なるとキッチン 五反田店", "唐揚げ・焼き鳥・肉", 1000, 1999, 5, "東京都品川区西五反田2丁目26−4",
 			"日曜日", "平日: 11:30〜15:00と17:00〜23:00、土曜・祝日: 16:00〜23:00");
-	Restaurant restaurant14 = new Restaurant(14, "SOSTA - PIZZA & LEGAME -", "イタリアン", 5000, 5900, 3,
+	Restaurant restaurant14 = new Restaurant(14, "SOSTA - PIZZA & LEGAME -", "イタリアン", 1000, 1999, 3,
 			"東京都品川区西五反田1丁目32−9",
 			"日曜日", "ランチ：11:30～15:00 ディナー：17:30～23:30");
-	Restaurant restaurant15 = new Restaurant(15, "もつ焼き 五反田 ふじ屋", "居酒屋・もつ鍋", 3000, 3900, 5,
+	Restaurant restaurant15 = new Restaurant(15, "もつ焼き 五反田 ふじ屋", "居酒屋・もつ鍋", 3000, 3999, 5,
 			"東京都品川区西五反田1丁目26−2 五反田サンハイツ101",
 			"なし", "月曜～木曜: 16:00～24:00 金曜: 16:00～25:00 土曜・日曜: 15:00～24:00");
-	Restaurant restaurant16 = new Restaurant(16, "七輪焼肉 安安 五反田西口店", "焼肉", 2000, 2900, 1, "東京都品川区西五反田2-7-8 誠実ビル 2F",
+	Restaurant restaurant16 = new Restaurant(16, "七輪焼肉 安安 五反田西口店", "焼肉", 2000, 2999, 1, "東京都品川区西五反田2-7-8 誠実ビル 2F",
 			"なし", "月～金：17:00～翌4:30 土・祝日：16:00～翌4:30 日：16:00～23:00");
-	Restaurant restaurant17 = new Restaurant(17, "ラーメン魁力屋 五反田店", "ラーメン", 1000, 1900, 1, "東京都品川区西五反田1丁目8−1",
+	Restaurant restaurant17 = new Restaurant(17, "ラーメン魁力屋 五反田店", "ラーメン", 1000, 1999, 1, "東京都品川区西五反田1丁目8−1",
 			"なし", "10:30 ～ 25:00");
-	Restaurant restaurant18 = new Restaurant(18, "一風堂 五反田東口店", "ラーメン", 1000, 1900, 1, "東京都品川区東五反田1丁目14−14 北原ビル 2F",
+	Restaurant restaurant18 = new Restaurant(18, "一風堂 五反田東口店", "ラーメン", 1000, 1999, 1, "東京都品川区東五反田1丁目14−14 北原ビル 2F",
 			"なし", "月曜日～金曜日：11:00～22:00 土曜日・日曜日：10:30～22:00");
-	Restaurant restaurant19 = new Restaurant(19, "つけめんTETSU 五反田店", "つけ麺・ラーメン", 1000, 1900, 1, "東京都品川区東五反田2丁目1−1",
+	Restaurant restaurant19 = new Restaurant(19, "つけめんTETSU 五反田店", "つけ麺・ラーメン", 1000, 1500, 1, "東京都品川区東五反田2丁目1−1",
 			"なし", "月〜土・祝前日: 11:00〜翌5:00 日・祝: 11:00〜23:00");
-	Restaurant restaurant20 = new Restaurant(20, "スシロー 五反田店", "寿司", 2000, 3000, 3, "東京都品川区西五反田1丁目3−8 五反田Placeビル B1",
+	Restaurant restaurant20 = new Restaurant(20, "スシロー 五反田店", "寿司", 2000, 2999, 3, "東京都品川区西五反田1丁目3−8 五反田Placeビル B1",
 			"なし", "月曜日～金曜日：11:00～23:00 土日祝日：10:30～23:00");
-	Restaurant restaurant21 = new Restaurant(21, "ラーメン豚山 五反田店", "ラーメン", 600, 1000, 1, "東京都品川区東五反田1丁目26−6",
+	Restaurant restaurant21 = new Restaurant(21, "ラーメン豚山 五反田店", "ラーメン", 950, 1350, 1, "東京都品川区東五反田1丁目26−6",
 			"なし", "11時00分～2時00分");
-	Restaurant restaurant22 = new Restaurant(22, "天下一品 五反田店", "ラーメン・中華", 600, 1000, 4, "東京都品川区西五反田1-25-5 松村ビル",
+	Restaurant restaurant22 = new Restaurant(22, "天下一品 五反田店", "ラーメン・中華", 940, 1320, 4, "東京都品川区西五反田1-25-5 松村ビル",
 			"なし", "月〜木：11:00〜翌2:00 金・祝前日：11:00〜翌3:00 土・日・祝：11:00〜23:00");
-	Restaurant restaurant23 = new Restaurant(23, "ミート矢澤", "肉・ハンバーグ", 2000, 7000, 5, "東京都品川区西五反田2丁目15−13 ニューハイツ西五反田 1F",
+	Restaurant restaurant23 = new Restaurant(23, "ミート矢澤", "肉・ハンバーグ", 2000, 2999, 5, "東京都品川区西五反田2丁目15−13 ニューハイツ西五反田 1F",
 			"年末年始", "[平日]ランチ 11:00～15:00 [土・日・祝] 11:00～23:30");
-	Restaurant restaurant24 = new Restaurant(24, "陳家私菜 五反田店", "中華", 1000, 2000, 3, "東京都品川区西五反田2-26-2 桔梗ハイツ2F",
+	Restaurant restaurant24 = new Restaurant(24, "陳家私菜 五反田店", "中華", 900, 1200, 3, "東京都品川区西五反田2-26-2 桔梗ハイツ2F",
 			"日曜日", "平日: ランチ 11:30～15:00 / ディナー 17:30～23:00 土曜・祝日: ランチ 11:30～15:00 / ディナー 17:30～21:30");
-	Restaurant restaurant25 = new Restaurant(25, "ONIGIRI CAFE ひとにぎり 五反田東急スクエア", "和食・おにぎり", 1000, 2000, 1,
+	Restaurant restaurant25 = new Restaurant(25, "ONIGIRI CAFE ひとにぎり 五反田東急スクエア", "和食・おにぎり", 1000, 1999, 1,
 			"東京都品川区東五反田2-1-2 五反田東急スクエア 4F",
 			"五反田東急スクエアに準ずる", "08:00～22:00");
-	Restaurant restaurant26 = new Restaurant(26, "アッパオンマ", "韓国料理", 3000, 4000, 3, "東京都品川区西五反田1丁目30−2 ウィン五反田ビル 1F",
+	Restaurant restaurant26 = new Restaurant(26, "アッパオンマ", "韓国料理", 4000, 4999, 3, "東京都品川区西五反田1丁目30−2 ウィン五反田ビル 1F",
 			"なし", "月～日、祝日、祝前日: 17:00～翌5:00 ディナータイム：17:00～22:00");
-	Restaurant restaurant27 = new Restaurant(27, "焼肉もとやま 五反田店", "焼肉", 6000, 7900, 1, "東京都品川区西五反田1丁目2−8 4F",
+	Restaurant restaurant27 = new Restaurant(27, "焼肉もとやま 五反田店", "焼肉", 6000, 7999, 1, "東京都品川区西五反田1丁目2−8 4F",
 			"なし", "16:00～23:00");
-	Restaurant restaurant28 = new Restaurant(28, "とんちゃん 五反田店", "焼肉・韓国料理", 3000, 3900, 2,
+	Restaurant restaurant28 = new Restaurant(28, "とんちゃん 五反田店", "焼肉・韓国料理", 3000, 3999, 2,
 			"東京都品川区東五反田5丁目28−12 EX五反田ビル 2F",
 			"なし", "7:00～翌5:00");
-	Restaurant restaurant29 = new Restaurant(29, "サイゼリヤ", "イタリアン", 1000, 2000, 4, "東京都品川区西五反田1丁目11−1 Aios 五反田駅前 2F",
+	Restaurant restaurant29 = new Restaurant(29, "サイゼリヤ", "イタリアン", 500, 1000, 4, "東京都品川区西五反田1丁目11−1 Aios 五反田駅前 2F",
 			"なし", "11:00～翌5:00");
-	Restaurant restaurant30 = new Restaurant(30, "マクドナルド", "ハンバーガー・ファストフード", 200, 1000, 1,
+	Restaurant restaurant30 = new Restaurant(30, "マクドナルド", "ハンバーガー・ファストフード", 500, 1000, 1,
 			"東京都品川区東五反田1丁目26−14 アトレ五反田1 2F・3F",
 			"なし", "24時間営業");
 
